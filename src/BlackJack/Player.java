3084cbe2dde48fa690194131f62d76535491a134
KyleLu1 <48026700+KyleLu1@users.noreply.github.com>
2019-04-05 19:32:52 -0700

    Update Player.java

--- src/BlackJack/Player.java
+++ src/BlackJack/Player.java
@@ -13,14 +13,20 @@ import java.util.Scanner;
 
 //define Player class here 
 
-
-
+public class Player extends AbstractPlayer
+{
+	private ArrayList<Card> hand;
+        private int winCount;
 
 	//constructors
-
-
-
-
+	public Player()
+	{
+            super();
+        }
 
 	//hit method goes here
-
+	public boolean hit()
+	{
+            return false;
+        }
+}
