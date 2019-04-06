5eeec756fdaa4aa782c8203365904247b3df55c5
KyleLu1 <48026700+KyleLu1@users.noreply.github.com>
2019-04-05 19:12:38 -0700

    Update AbstractPlayer.java

--- src/BlackJack/AbstractPlayer.java
+++ src/BlackJack/AbstractPlayer.java
@@ -18,38 +18,33 @@ public abstract class AbstractPlayer implements Playerable
 
    public  void addCardToHand( Card temp )
    {
-
-
+	hand.add(temp);
    }
 
    public  void resetHand( )
    {
-
-
+	hand.clear();
    }
 
    public  void setWinCount( int numwins )
-   {
-
-
+   {	
+   	winCount = numwins;
    }
 
-   public int getWinCount() { return 0; }
+   public int getWinCount() { return winCount; }
 
-   public int getHandSize() { return 0; }
+   public int getHandSize() { return hand.size; }
 
    public int getHandValue()
    {
 		//great example of polymorphism
       int total=0;
-
-
-
-
-
-
-
+      for(int i = 0; i < getHandSize(); i++)
+      {
+          total += hand.get(i).getValue();
+      }
       return total;
+
    }
 
    public String toString()
