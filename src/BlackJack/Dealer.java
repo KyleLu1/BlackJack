69ec28d2e81c509babcdee0cd37b85e151243c87
KyleLu1 <48026700+KyleLu1@users.noreply.github.com>
2019-04-05 19:35:16 -0700

    Update Dealer.java

--- src/BlackJack/Dealer.java
+++ src/BlackJack/Dealer.java
@@ -8,33 +8,36 @@ package BlackJack;
 
 
 //define Dealer class here
-	
-	
-	
-	
+public class Dealer extends AbstractPlayer
+{
 	//instance variable - Deck 
-
-
-
-
+        Deck d;
 
 	//constructors
-
+        public Dealer(){
+            d = new Deck();
+        }
 
 
 
 
 	//method to shuffle
-
+        public void shuffle(){
+            d.shuffle();
+        }
 
 
 
 
 
 	//method to deal a card
-
-
-
+        public Card deal(){
+            return d.nextCard();
+        }
 
 
 	//hit method goes here
+        public boolean hit(){
+            return getHandValue() < 17;
+        }
+}
